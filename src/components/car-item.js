5cfa69156b91258f377a182bae26ff54db6f01bc
Artem <gusev2014russia@mail.ru>
2019-08-11 16:06:28 +0300

    load data from json file

--- src/components/car-item.js
+++ src/components/car-item.js
@@ -1,34 +1,46 @@
 import {PolymerElement, html} from '@polymer/polymer/polymer-element';
+import '@granite-elements/granite-bootstrap/granite-bootstrap.js';
 
 // eslint-disable-next-line require-jsdoc
 class carItem extends PolymerElement {
   // eslint-disable-next-line require-jsdoc
   static get template() {
     return html`
+    <style include="granite-bootstrap"></style>
       <style>
         .car {
             margin: 10px;
             padding: 10px;
             border: 1px solid black;
             border-radius: 10px;
-            min-height: 50px;
+            min-height: 150px;
         }
-        h2 {
-            color: red
+        .el-img {
+            max-height: 100px;
+        }
+        .el-country, .el-max-speed{
+            clear: both;
         }
       </style>
       
-      <div class="car">
-        <h2>[[name]]</h2>
-        <p>[[description]]</p>
-        <p>Страна: [[country]]</p>
-        <p>Макс. скрость: [[speed]] km/h</p>
+      <div class="car clearfix" id="[[id]]">
+        <img src="[[img]]" alt="[[name]]" class="float-right el-img">
+        <h2 class="el-name">[[name]]</h2>
+        <p class="el-description">[[description]]</p>
+        <p class="float-right el-country">Country: [[country]]</p>
+        <p class="float-right el-max-speed">Max. speed: [[speed]] km/h</p>
       </div>   
     `;
   }
 
   static get properties() {
     return {
+      id: {
+        type: String,
+      },
+      img: {
+        type: String,
+      },
       name: {
         type: String,
       },
--- src/components/cars-list.js
+++ src/components/cars-list.js
@@ -47,7 +47,9 @@ class carsList extends PolymerElement {
                   name="[[car.name]]" 
                   description=[[car.description]]" 
                   country="[[car.country]]" 
-                  speed="[[car.speed]]">
+                  speed="[[car.speed]]"
+                  id="[[car.id]]"
+                  img="[[car.image]]">
                 </car-item>
               </template>
             </div>
@@ -82,39 +84,24 @@ class carsList extends PolymerElement {
   constructor() {
     super();
 
+    this.cars = [];
+    this._getData();
+
     this.criteria = [
       {name: 'name', label: 'Alphabetical'},
       {name: 'speed', label: 'Max speed'},
     ];
 
     this.criterium = this.criteria[0].name;
+  }
 
-    this.cars = [
-      {
-        name: 'Mitsubishi 3000GT',
-        description: 'Mitsubishi 3000GT — спортивный автомобиль класса GT японской компании Mitsubishi Motors. Выпускался с 1990 по 2000 год. На внутреннем рынке Японии был известен как Mitsubishi GTO. Для Северной Америки автомобиль был дособран корпорацией Chrysler и получил название Dodge Stealth.',
-        country: 'Япония',
-        speed: 250,
-      },
-      {
-        name: 'Toyota Corolla AE86',
-        description: 'Toyota AE86, также известная, как Toyota Corolla Levin и Toyota Sprinter Trueno, - легкий (923 кг) автомобиль в кузове хетчбек или купе, производящийся с 1983 года по 1987 год компанией Toyota. Для краткости код шасси «AE86» обозначает 1600-кубовую заднеприводную модель.',
-        country: 'Япония',
-        speed: 180,
-      },
-      {
-        name: 'Toyota Mark II',
-        description: 'Toyota Mark II (яп. トヨタ・マークII) — среднеразмерный седан, выпускавшийся компанией Toyota с 1968 по 2004 годы. Наименование Mark II использовалось компанией Toyota на протяжении нескольких десятилетий и первоначально использовалось в составе названия Toyota Corona Mark II. Отметка II была введена, чтобы машина выделялась из основной платформы Toyota Corona. Как только в 1970-е годы платформа была разделена, автомобиль стал известен просто как Mark II.',
-        country: 'Япония',
-        speed: 240,
-      },
-      {
-        name: 'Toyota Crown',
-        description: 'Toyota Crown (яп. トヨタクラウン Toyota Kuraun) — автомобиль производства компании Toyota, превративший эту модель в линейку полноразмерных седанов класса «люкс». Первоначально продавались в Японии и некоторых других азиатских странах, изначально разрабатываясь для эксплуатации в качестве такси. Так было в последние годы продаж в США, где автомобиль продавали с конца 1950-х годов и по 1971. Crown — старейший седан, всё ещё выпускаемый Toyota. По социальному статусу он конкурирует лишь с Toyota Century, Toyota Celsior и Toyota Crown Majesta. Crown используется многими японскими организациями в качестве лимузинов компании.',
-        country: 'Япония',
-        speed: 260,
-      },
-    ];
+  async _getData() {
+    try {
+      const res = await fetch('./data/cars.json');
+      this.cars = await res.json();
+    } catch (e) {
+      throw new Error(e);
+    }
   }
 
   _inputChange() {
